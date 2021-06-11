10c962e4a23eeb8dfdd91750c0c060100d582666
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-11 23:49:09 +0300

    feat: add first task rxjs

--- rxjs-task/index.js
+++ rxjs-task/index.js
@@ -2,8 +2,23 @@
     const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
     const getApiResponse = (searchString) => new Promise(resolve =>
-            setTimeout(() => resolve(searchString), getRandomInt(1, 10) * 500)
+        setTimeout(() => resolve(searchString), getRandomInt(1, 10) * 500)
     );
 
+    const { fromEvent } = rxjs;
+
+    const button = document.querySelector('#button');
+    const input = document.querySelector('#input');
+    const responsiveContainer = document.querySelector('#response-container');
+    const requestContainer = document.querySelector('#cancel-prev-request-container');
+
+    // task 1 
+    fromEvent(button, 'click').subscribe(() => {
+        input.value = '';
+        responsiveContainer.textContent = '';
+        requestContainer.textContent = '';
+    });
+
+    // task 2
     
 })()
