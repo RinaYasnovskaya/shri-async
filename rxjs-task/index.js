f391bfb86671a21605aebcb9059b901fa3c0a854
Rina Yasnovskaya <rina.yasnovskaya@gmail.com>
2021-06-12 17:31:05 +0300

    feat: add task 2 in rxjs

--- rxjs-task/index.js
+++ rxjs-task/index.js
@@ -5,7 +5,8 @@
         setTimeout(() => resolve(searchString), getRandomInt(1, 10) * 500)
     );
 
-    const { fromEvent } = rxjs;
+    const { fromEvent, from } = rxjs;
+    const { switchMap } = rxjs.operators;
 
     const button = document.querySelector('#button');
     const input = document.querySelector('#input');
@@ -13,12 +14,24 @@
     const requestContainer = document.querySelector('#cancel-prev-request-container');
 
     // task 1 
-    fromEvent(button, 'click').subscribe(() => {
+    const buttonObservable$ = fromEvent(button, 'click');
+
+    buttonObservable$.subscribe(() => {
         input.value = '';
         responsiveContainer.textContent = '';
         requestContainer.textContent = '';
     });
 
     // task 2
-    
+    const inputObservable$ = fromEvent(input, 'input');
+
+    const resolveCancelRequest = () => {
+        inputObservable$
+            .pipe((switchMap(ev => from(getApiResponse(ev.target.value)))))
+            .subscribe(result => {
+                requestContainer.textContent = result;
+            })
+    };
+
+    resolveCancelRequest();
 })()
